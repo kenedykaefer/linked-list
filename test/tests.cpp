c5bfa034d27ec48b6cfc9fe657dc85621d0a7c37
Kenedy Kaefer <kenedykae@gmail.com>
2023-02-05 14:32:53 -0300

    update tests

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,12 +1,23 @@
 #include <catch2/catch_test_macros.hpp>
-#include <catch2/generators/catch_generators.hpp>
 
 #include "list.hpp"
 
-TEST_CASE("empty list", "[empty]")
+TEST_CASE("empty list", "[capacity],[empty]")
 {
     LinkedList::List<int> list;
 
-    REQUIRE(list.isEmpty());
-    REQUIRE(list.size() == 0);
+    SECTION("new list")
+    {
+        REQUIRE(list.isEmpty() == true);
+    }
+}
+
+TEST_CASE("list size", "[capacity],[size]")
+{
+    LinkedList::List<int> list;
+
+    SECTION("new list")
+    {
+        REQUIRE(list.size() == 0);
+    }
 }
